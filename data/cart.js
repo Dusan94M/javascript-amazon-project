85588a481534dfd439116b8d35009fd8892cbc03
Dusan <du_le94@icloud.com>
2025-01-06 00:35:17 +0100

    Delete products from cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },{
@@ -6,7 +6,7 @@ export const cart = [{
   quantity: 1
 }];
 
-export function addToCart(productId){
+export function addToCart(productId) {
 
   let quantitySelector = document.querySelector(`.js-selector${productId}`);
 
@@ -28,4 +28,16 @@ export function addToCart(productId){
       quantity
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if( cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
