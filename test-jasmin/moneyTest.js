8929cd340521313e8aeb36d7033e103bd357c4e7
Dusan <du_le94@icloud.com>
2025-02-10 23:09:00 +0100

    Remove default test and made money.js test

---
+++ test-jasmin/moneyTest.js
@@ -0,0 +1,15 @@
+import { formatCurrency } from"../script/utils/money.js";
+
+describe('test suit: formatCurrency', () => {
+  it('It works with 20.95?', () => {
+    expect(formatCurrency(2095)).toEqual('20.95')
+  });
+});
+
+it('it works with 0', () => {
+  expect(formatCurrency(0)).toEqual('0.00')
+});
+
+it('It will round nearest number', () => {
+  expect(formatCurrency(2000.5)).toEqual('20.01')
+});
