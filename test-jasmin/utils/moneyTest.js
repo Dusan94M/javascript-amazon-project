a3bfd3aff516d3727864b0c9e0945bac5d7fcebe
Dusan <du_le94@icloud.com>
2025-02-12 18:53:25 +0100

    Done with moneyTest.js

--- test-jasmin/utils/moneyTest.js
+++ test-jasmin/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from"../script/utils/money.js";
+import { formatCurrency } from"../../script/utils/money.js";
 
 describe('test suit: formatCurrency', () => {
   it('It works with 20.95?', () => {
