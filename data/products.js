1e48e73e5ef20037484e57e3be84cf0fccb10ab3
Dusan <du_le94@icloud.com>
2025-02-24 07:01:09 +0100

    Convert products in to classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../script/utils/money.js";
 export function getProduct(productId){
   let matchingProducts;
 
@@ -8,6 +9,30 @@ export function getProduct(productId){
   });
 
   return matchingProducts;
+}
+
+class Products {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStartURL(){
+    return `src="images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
 }
 
 export const products = [
@@ -669,4 +694,8 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Products(productDetails);
+});
+
+console.log(products);
--- script/checkout.js
+++ script/checkout.js
@@ -1,8 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-
-import {Cart} from '../data/cart-class.js';
+//import {Cart} from '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
