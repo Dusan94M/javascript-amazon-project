fb4d83827991472a1d4316b0a3eee7b0fa952df8
Dusan <du_le94@icloud.com>
2025-02-27 00:58:49 +0100

    add backend-practice.js file

--- script/checkout.js
+++ script/checkout.js
@@ -3,7 +3,7 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 //import {Cart} from '../data/cart-class.js';
 //import { Car } from"../data/car.js"
-
+import'../data/backend-practice.js'
 renderOrderSummary();
 renderPaymentSummary();
 renderCheckoutHeader();
