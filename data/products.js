e5a2c0c70c836b5d9e18c1aa76ae37e0585dca47
Dusan <du_le94@icloud.com>
2025-03-03 02:36:57 +0100

    Convert loadProducts from XHR HTTP request to fetch

--- data/products.js
+++ data/products.js
@@ -89,6 +89,28 @@ obj3.method();
 
 */
 
+export function loadProductsFetch(){
+  let promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      if(productDetails.type === 'appliance'){
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products')
+  });
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('load fetch')
+});
+*/
 export let products = [];
 
 export function loadProducts(fun){
