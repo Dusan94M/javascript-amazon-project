b19b713cc9f5d953c45de48174f3c75e33f5a748
Dusan <du_le94@icloud.com>
2025-03-12 22:55:32 +0100

    Practicing promises

--- data/cart.js
+++ data/cart.js
@@ -118,3 +118,14 @@ export function loadCart(fun){
   xhr.send();
 
 }
+
+export async  function loadCartFetch(){
+  const response = await fetch('https://supersimplebackend.dev/cart', {
+    method: 'GET'
+  })
+  const text = response.text();
+  console.log(text)
+  return text;
+}
+
+loadCartFetch();
--- script/checkout.js
+++ script/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
-import { loadCart } from '../data/cart.js';
+import { loadCart, loadCartFetch } from '../data/cart.js';
 //import {Cart} from '../data/cart-class.js';
 //import { Car } from"../data/car.js";
 //import'../data/backend-practice.js';
@@ -10,14 +10,11 @@ async function loadPage(){
  try{
 //  throw 'error1';
   await loadProductsFetch();
-
-   await new Promise((resolve, reject) => {
-    //throw 'error2'
-    loadCart(() => {
-      //reject('error3');
-      resolve();
-    });
-  })
+  await loadCartFetch();
+  await Promise.all([
+    loadProductsFetch(),
+    loadCartFetch()
+  ])
  } 
  catch(error){
   console.log('Unexpected error, please try again!');
